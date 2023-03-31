c4cdef046a1dfb0174da2138af0b9620f18476df
Xritre <98012222+Xritre@users.noreply.github.com>
2023-03-31 20:00:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,38 @@
-const express        = require('express'),
-      app            = express(),
-      bodyParser     = require("body-parser")
-      mongoose       = require("mongoose"),
-      passport       = require("passport");
-      LocalStrategy  = require("passport-local"),
-      flash          = require('connect-flash'),
-      methodOverride   = require('method-override'),
-      Artist        = require("./models/artist"),
-      Song             = require("./models/song"),
-      User           = require("./models/user"),
-      seedDB         = require("./seeds.js")
-    
-      
+const express = require('express'),
+    app = express(),
+    bodyParser = require("body-parser")
+mongoose = require("mongoose"),
+    passport = require("passport");
+LocalStrategy = require("passport-local"),
+    flash = require('connect-flash'),
+    methodOverride = require('method-override'),
+    Artist = require("./models/artist"),
+    Song = require("./models/song"),
+    User = require("./models/user"),
+    seedDB = require("./seeds.js")
+
+
 
 const indexRoutes = require('./routes/index'),
-       artistRoutes = require('./routes/artist'),
-      songRoutes = require('./routes/song')
-    //   ,topFavRoutes = require('./routes/topfav')
+    artistRoutes = require('./routes/artist'),
+    songRoutes = require('./routes/song')
+//   ,topFavRoutes = require('./routes/topfav')
 
-mongoose.connect('mongodb://localhost/HutaoMusic');      
+mongoose.connect('mongodb://127.0.0.1:27017/HutaoMusic');
 app.set("view engine", "ejs");
 app.use(express.static("./public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-//seedDB();
+seedDB();
+
+
 
 app.use(require('express-session')
-({
-    secret: 'secret word',
-    resave: false,
-    saveUnintialized: false
-}))
+    ({
+        secret: 'secret word',
+        resave: false,
+        saveUninitialized: false
+    }))
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -40,8 +42,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
-app.use(function(req,res,next)
-{
+app.use(function (req, res, next) {
     res.locals.currentUser = req.user;
     res.locals.error = req.flash('error')
     res.locals.success = req.flash('success')
@@ -53,6 +54,6 @@ app.use('/', indexRoutes);
 app.use('/songs', songRoutes);
 app.use('/artists', artistRoutes);
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Activated");
 });
